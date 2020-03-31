6b9e4cbd3c8a352c1cef96e901dc558dadf99ce2
lomomeno <menocorde04@gmail.com>
2020-03-31 16:30:43 -0600

    todo bien, falta destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+    acts_as_paranoid
     validates :title, presence: true,
     length: { minimum: 5 }
+
+
+    def self.from_today
+        where("'created_at' > #{Date.today - 1.day}")
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327212120) do
+ActiveRecord::Schema.define(version: 20200331024139) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_articles_on_deleted_at"
   end
 
 end
